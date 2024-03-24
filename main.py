5f14c7367dc635c4ebb0cf41f0db4b32aac559ea
JanVych <132760222+JanVych@users.noreply.github.com>
2024-03-25 00:04:33 +0100

    communication testing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
 import socket
+
+import json
+
+import os
+
 import etatherm
 
 from machine import Pin
@@ -24,11 +29,10 @@ if __name__ == "__main__":
     wlan = Wlan()
     config = Config("config.json")
 
-
     def try_connect():
-        networks = config.get("networks")
-        for n in networks:
-            wlan.wifi_connect(n["ssid"], n["password"])
+        network_list = config.get("networks")
+        for nw in network_list:
+            wlan.wifi_connect(nw["ssid"], nw["password"])
             if wlan.wifi.isconnected():
                 print("connected")
                 return
@@ -40,6 +44,7 @@ if __name__ == "__main__":
     s.bind(('', 34197))
     s.listen(5)
 
+    conn = None
     while True:
         button_state = button.value()
         # led.value(button_state)
@@ -50,10 +55,70 @@ if __name__ == "__main__":
             wlan.wifi_disconnect()
             wlan.access_point_up()
         if wlan.access_point.active():
-            conn, addr = s.accept()
-            print(f"Got a connection from {str(addr)}")
+            if conn is None:
+                print("waiting to connection")
+                conn, addr = s.accept()
+                print(f"Got a connection from {str(addr)}")
             data = conn.recv(1024)
             print(f"Content = {str(data)}")
 
-            conn.send(b"Hello from ESP")
+            json_data = json.loads(data)
+            try:
+                request = json_data["header"]
+            except KeyError:
+                print("unknown data")
+                continue
+
+            if request == "getInfo":
+                print("request for info")
+                info = os.uname()
+                networks = []
+                for n in wlan.available_networks():
+                    if n[0]:
+                        networks.append(n[0].decode("utf-8"))
+                networks_string = ",".join(networks)
+
+                response = {"deviceId": config.get("deviceId"),
+                            "deviceName": config.get("deviceName"),
+                            "networks": networks_string,
+                            "sysName": info[0],
+                            "nodeName": info[1],
+                            "release": info[2],
+                            "version": info[3],
+                            "machine": info[4]
+                            }
+
+                conn.send(json.dumps(response))
+                print(response)
+                continue
+            if request == "setDataAndConnect":
+                print("set data and connect")
+                print(json_data)
+                config.set("deviceId", json_data["deviceId"])
+                config.set("deviceName", json_data["deviceName"])
+                ssid = json_data["wifiSsid"]
+                password = json_data["wifiPassword"]
+
+                network_list = config.get("networks")
+                for index, n in enumerate(network_list):
+                    if n["ssid"] == ssid:
+                        n["password"] = password
+                        break
+                    if index == len(network_list) - 1:
+                        network_list.append({"ssid": ssid, "password": password})
+                        break
+
+                config.set("networks", network_list)
+                wlan.access_point_down()
+                wlan.wifi_connect(ssid, password)
+                conn = None
+                if wlan.wifi.isconnected():
+                    print(f"connected to {ssid}")
+                    continue
+
+
+
+
+
+            # conn.send(b"Hello from ESP")
         sleep(0.1)
