af312b27b263a2b8b70540eb902c424d55e5a728
JanVych <132760222+JanVych@users.noreply.github.com>
2024-03-31 03:38:37 +0200

    async and threading test

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-import json
-
-import os
+from os import remove
+from json import loads as json_loads
+from json import dumps as json_dumps
 
 
 class Config:
@@ -14,10 +14,10 @@ class Config:
             print(f"cant open file: {self.file_path}")
             return
         try:
-            self.config = json.loads(config_string)
+            self.config = json_loads(config_string)
         except ValueError:
             config_string = "{}"
-            self.config = json.loads(config_string)
+            self.config = json_loads(config_string)
         self.__save_file(config_string)
 
     def __save_file(self, json_string: str) -> None:
@@ -29,17 +29,17 @@ class Config:
 
     def __delete_file(self) -> None:
         try:
-            os.remove(self.file_path)
+            remove(self.file_path)
         except Exception as exp:
             print(f"error: {exp}, file:{self.file_path} ")
 
     def clear_config(self) -> None:
         self.__save_file("{}")
-        self.config = json.loads("{}")
+        self.config = json_loads("{}")
 
     def set(self, key: str, value: object) -> None:
         self.config[key] = value
-        self.__save_file(json.dumps(self.config))
+        self.__save_file(json_dumps(self.config))
 
     def get(self, key: str):
         if self.config:
--- main.py
+++ main.py
@@ -1,138 +1,138 @@
-import sys
-import socket
-
-import json
-
-import os
-from gc import mem_free
-import _thread
-
-import etatherm
-
-from machine import Pin
-from time import sleep
-
 from config import Config
-
 from wlan import Wlan
 
-# wifi_ssid = "TP-Link-29"
-# wifi_password = "ac41.E9lz77"
+from _thread import start_new_thread, exit
+from asyncio import start_server, StreamReader, StreamWriter, sleep
+from asyncio import run as asyncio_run
+from json import loads as json_loads
+from json import dumps as json_dumps
 
-# access_point_password = "123456789"
+from machine import Pin
+from os import uname
+from gc import mem_free, mem_alloc, collect
 
-if __name__ == "__main__":
+from time import sleep as tsleep
+
+thread_flag = False
+server_mode = True
+
+
+def thread_func():
+    b_led = Pin(2, Pin.OUT)
+    while thread_flag:
+        print(f"allocated: {mem_alloc()} B")
+        print(f"free: {mem_free()} B")
+        b_led.value(1)
+        tsleep(1)
+        b_led.value(0)
+        tsleep(1)
+    exit()
+
+
+async def handle_connection(reader: StreamReader, writer: StreamWriter,
+                            wlan: Wlan, config: Config):
+    print("handle connection")
+    global server_mode
+    data_bytes = await reader.read()
+    data = data_bytes.decode()
+    json_data = None
+    header = None
+    try:
+        json_data = json_loads(data)
+    except ValueError:
+        print("cant convert to json")
+
+    if json_data:
+        try:
+            header = json_data["header"]
+            print(f"header: {header}")
+        except KeyError:
+            print("no data header")
+
+        if header == "getInfo":
+            info = uname()
+            networks = []
+            for n in wlan.available_networks():
+                if n[0]:
+                    networks.append(n[0].decode())
+            networks_string = ",".join(networks)
+
+            response = {"deviceId": config.get("deviceId"),
+                        "deviceName": config.get("deviceName"),
+                        "networks": networks_string,
+                        "sysName": info[0],
+                        "nodeName": info[1],
+                        "release": info[2],
+                        "version": info[3],
+                        "machine": info[4]
+                        }
+            writer.write(json_dumps(response).encode())
+            print(f"send: {response}")
+
+    if header == "setDataAndConnect":
+        print(f"data: {json_data}")
+        config.set("deviceId", json_data["deviceId"])
+        config.set("deviceName", json_data["deviceName"])
+        ssid = json_data["wifiSsid"]
+        password = json_data["wifiPassword"]
+        network_list = config.get("networks")
+
+        for index, n in enumerate(network_list):
+            if n["ssid"] == ssid:
+                n["password"] = password
+                break
+            if index == len(network_list) - 1:
+                network_list.append({"ssid": ssid, "password": password})
+                break
+
+        config.set("networks", network_list)
+        server_mode = False
+        wlan.wifi_connect(ssid, password)
+
+    await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    wlan = Wlan()
+    config = Config("config.json")
+    server = None
+    global server_mode
+    global thread_flag
+    pid = start_new_thread(thread_func, ())
+
     led = Pin(14, Pin.OUT)
     button = Pin(27, Pin.IN)
 
-    # etatherm = etatherm.Etatherm(16, 17)
-
-    wlan = Wlan()
-    config = Config("config.json")
-
-
-    def try_connect():
-        network_list = config.get("networks")
-        for nw in network_list:
-            wlan.wifi_connect(nw["ssid"], nw["password"])
-            if wlan.wifi.isconnected():
-                print("connected")
-                return
-
-
-    thread_active = True
-
-    def led_blink_loop():
-        b_led = Pin(2, Pin.OUT)
-        while thread_active:
-            b_led.value(1)
-            sleep(1)
-            b_led.value(0)
-            sleep(1)
-
-
-    pid = _thread.start_new_thread(led_blink_loop, ())
-
-    try_connect()
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 34197))
-    s.listen(5)
-
-    conn = None
     while True:
-        # print(f"free: {mem_free()} B")
+        await sleep(0)
         button_state = button.value()
-        # led.value(button_state)
-        # if button_state:
-        #     print("try read real temperature")
-        #     print(etatherm.get_real_temperature(8))
         if button_state:
+            print("button pressed")
+            led.value(1)
+            server_mode = True
+            #thread_flag = False
             wlan.wifi_disconnect()
-            wlan.access_point_up()
-        if wlan.access_point.active():
-            if conn is None:
-                print("waiting to connection")
-                conn, addr = s.accept()
-                print(f"Got a connection from {str(addr)}")
-            data = conn.recv(1024)
-            print(f"Content = {str(data)}")
+            collect()
+            await sleep(1)
+            led.value(0)
 
-            json_data = json.loads(data)
-            try:
-                request = json_data["header"]
-            except KeyError:
-                print("unknown data")
-                continue
+        if server_mode:
+            if not server:
+                wlan.access_point_up()
+                server = await start_server(lambda r, w: handle_connection(r, w, wlan, config),
+                                            "", 34197)
+                print("server listening")
 
-            if request == "getInfo":
-                print("request for info")
-                info = os.uname()
-                networks = []
-                for n in wlan.available_networks():
-                    if n[0]:
-                        networks.append(n[0].decode("utf-8"))
-                networks_string = ",".join(networks)
-
-                response = {"deviceId": config.get("deviceId"),
-                            "deviceName": config.get("deviceName"),
-                            "networks": networks_string,
-                            "sysName": info[0],
-                            "nodeName": info[1],
-                            "release": info[2],
-                            "version": info[3],
-                            "machine": info[4]
-                            }
-
-                conn.send(json.dumps(response))
-                print(response)
-                conn.close()
-                conn = None
-                continue
-            if request == "setDataAndConnect":
-                print("set data and connect")
-                print(json_data)
-                config.set("deviceId", json_data["deviceId"])
-                config.set("deviceName", json_data["deviceName"])
-                ssid = json_data["wifiSsid"]
-                password = json_data["wifiPassword"]
-
-                network_list = config.get("networks")
-                for index, n in enumerate(network_list):
-                    if n["ssid"] == ssid:
-                        n["password"] = password
-                        break
-                    if index == len(network_list) - 1:
-                        network_list.append({"ssid": ssid, "password": password})
-                        break
-
-                config.set("networks", network_list)
+        else:
+            if server:
                 wlan.access_point_down()
-                wlan.wifi_connect(ssid, password)
-                conn.close()
-                conn = None
-                if wlan.wifi.isconnected():
-                    print(f"connected to {ssid}")
-                    continue
+                server.close()
+                await server.wait_closed()
+                server = None
+                print("server killed")
 
-        sleep(0.1)
+
+if __name__ == "__main__":
+    asyncio_run(main())
